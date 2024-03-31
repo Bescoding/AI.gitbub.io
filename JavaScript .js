b60d6ac73447ed71fb23994ed8958684736bcac2
Bescoding <165542312+Bescoding@users.noreply.github.com>
2024-03-31 08:45:46 +0300

    Add files via upload

---
+++ JavaScript .js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('h1').addEventListener('click', function() {
+        alert('Welcome to the Introduction to Artificial Intelligence page!');
+    });
+});
